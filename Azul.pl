669471ee804cc640be2e1dd65898dd7d57e0d081
JPovedanoR <38103406+JPovedanoR@users.noreply.github.com>
2020-02-20 08:54:46 +0100

    reglas actualizadas

--- Azul.pl
+++ Azul.pl
@@ -1,15 +1,15 @@
 % Autor:
 % Fecha: 20/02/2020
-l
 
 
-%lista de la bolsa de fichas (10 unidades de cada ficha)
+
+%lista de la bolsa de fichas (20 unidades de cada ficha)   y 100 fichas en total (azul amarillo rojo negro y blanco)
 %las fichas se reparten de forma aleatoria a la mesa y meterlas a las factorias de la mesa, cuatro para cada una (la factoria una lista de 4 elementos)
 %otra lista del centro de la mesa, añadiendo las fichas que se quitan de las factorias,
 %estado completo del juego guardado en una lista que contenga todo
 
 %tablero de jugador con tres sublistas, lineas de patron(5x5 pero con restricciones), mosaico (5x5) y la linea de suelo (siete posiciones)
-%segun el numero de jugadores cambia el numero de tablero
+%segun el numero de jugadores cambia el numero de tablero para 2->5 3->7 4->9
 
 %DE QUE FACTORIA COGE COLOR Y QUE LINEA DE PATRON QUIERE COLOCAR (menu)
 %presentar las factorias
@@ -19,8 +19,8 @@ l
 %en el centro quedan las sobrantes, un jugador puede coger un color entero del centro de la mesa
 %cuando hay un color en una linea de patron este se debe mantener en toda la linea, si coges baldosas de mas que no caben en esa linea las sobrantes  pasan al suelo(papelera)
 
-%cuando este todo relleno completamos el mosaico, se vacian las lineas de patron y se mueven al mosaico rellenando
+%cuando se acaben las fichas en la mesa completamos el mosaico, se vacian las lineas de patron y se mueven al mosaico rellenando solo el colo r de la linea que corresponda de la linea de patron al mosaico (solo se mueven las lineas que esten completas)
 %teniendo el mosaico relleno de minusculas marcando como que esta vacio y al rellenar las pasamos a mayusculas, rellenando las lineas por las diagonales
-%se cuentan los puntos y se reinicia el juego hasta que un jugador hace una fila completa
+%se reinicia el juego hasta que un jugador hace una fila completa
 
 %NO HAY QUE HACER MEMORIA SOLO HAY QUE HACER UN FOLIO CON LOS NOMBRES Y HASTA DONDE SE HA LLEGADO.
