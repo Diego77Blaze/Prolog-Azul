870fdbffe6c89c6ef2a21a7dc3c114b5c5c66f01
JPovedanoR <38103406+JPovedanoR@users.noreply.github.com>
2020-02-20 08:42:58 +0100

    Update Azul.pl

--- Azul.pl
+++ Azul.pl
@@ -1,3 +1,26 @@
 % Autor:
 % Fecha: 20/02/2020
+l
 
+
+%lista de la bolsa de fichas (10 unidades de cada ficha)
+%las fichas se reparten de forma aleatoria a la mesa y meterlas a las factorias de la mesa, cuatro para cada una (la factoria una lista de 4 elementos)
+%otra lista del centro de la mesa, añadiendo las fichas que se quitan de las factorias,
+%estado completo del juego guardado en una lista que contenga todo
+
+%tablero de jugador con tres sublistas, lineas de patron(5x5 pero con restricciones), mosaico (5x5) y la linea de suelo (siete posiciones)
+%segun el numero de jugadores cambia el numero de tablero
+
+%DE QUE FACTORIA COGE COLOR Y QUE LINEA DE PATRON QUIERE COLOCAR (menu)
+%presentar las factorias
+
+%cada turno un jugador coge un color de una factoria (si hay dos de un mismo color coge ambas ) el resto de baldosas que no sean del color que ha elegido van al centro de la mesa eliminadas
+%despues esas baldosas las tiene que poner en las lineas de patron
+%en el centro quedan las sobrantes, un jugador puede coger un color entero del centro de la mesa
+%cuando hay un color en una linea de patron este se debe mantener en toda la linea, si coges baldosas de mas que no caben en esa linea las sobrantes  pasan al suelo(papelera)
+
+%cuando este todo relleno completamos el mosaico, se vacian las lineas de patron y se mueven al mosaico rellenando
+%teniendo el mosaico relleno de minusculas marcando como que esta vacio y al rellenar las pasamos a mayusculas, rellenando las lineas por las diagonales
+%se cuentan los puntos y se reinicia el juego hasta que un jugador hace una fila completa
+
+%NO HAY QUE HACER MEMORIA SOLO HAY QUE HACER UN FOLIO CON LOS NOMBRES Y HASTA DONDE SE HA LLEGADO.
